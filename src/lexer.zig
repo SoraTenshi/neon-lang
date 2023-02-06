03dd6f0408a1563a772f6604fb91b398e029a907
Sora <dream@neoncity.dev>
2023-02-06 23:20:57 +0000

    Add a few more tests, that deal more complex scenarios

--- src/lexer.zig
+++ src/lexer.zig
@@ -226,3 +226,30 @@ test "string literal parsing" {
         try t.expectEqualStrings(expected_tokens[value].value, actual_tokens[value].value);
     }
 }
+
+test "function decl with body" {
+    const str = "main :: (): string {\n}";
+    var lexer = Lexer.init(t.allocator, str);
+
+    const expected_tokens = &[_]parser.Token{
+        .{ .token = .identifier, .value = "main" },
+        .{ .token = .fn_decl, .value = "" },
+        .{ .token = .left_paren, .value = "" },
+        .{ .token = .right_paren, .value = "" },
+        .{ .token = .colon, .value = "" },
+        .{ .token = .string, .value = "" },
+        .{ .token = .left_brace, .value = "" },
+        .{ .token = .right_brace, .value = "" },
+    };
+
+    const actual_tokens = try lexer.tokenize();
+    defer lexer.alloc.free(actual_tokens);
+
+    try t.expectEqual(expected_tokens.len, actual_tokens.len);
+
+    var value: usize = 0;
+    while (value < expected_tokens.len) : (value += 1) {
+        try t.expectEqual(expected_tokens[value].token, actual_tokens[value].token);
+        try t.expectEqualStrings(expected_tokens[value].value, actual_tokens[value].value);
+    }
+}
